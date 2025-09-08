4447b521d0659ef3858a72da46f21cf194fa1c6a
qiramo <>
2025-09-08 16:20:15 +0200

    add: chromium

---
+++ chromium/Default/Extensions/bgnkhhnnamicmpeenaelnjfhikgbkllg/5.1.141_0/vendors/scriptlets.js
@@ -0,0 +1,24 @@
+"use strict";
+(self["webpackChunkbrowser_extension"] = self["webpackChunkbrowser_extension"] || []).push([[728],{
+
+/***/ 61702:
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+/* unused harmony export SCRIPTLETS_VERSION */
+
+
+var version = "2.1.6";
+
+var SCRIPTLETS_VERSION = (/* unused pure expression or super */ null && (version));
+
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__(61702));
+/******/ }
+]);
